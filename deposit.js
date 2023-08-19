49519d94faa9a53df9756459af233d2021902aba
CoderMuhib10 <kidbecomingcreative12@gmail.com>
2023-08-19 17:39:28 +0600

    fiinishh added validation and re-checked evrything and made it better

--- deposit.js
+++ deposit.js
@@ -5,11 +5,15 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
   const depositField = document.getElementById("deposit-field");
   const newDepositAmountString = depositField.value;
   const newDepositAmount = parseFloat(newDepositAmountString);
-  //   const type = typeof newDepositAmount;
-  //   console.log(type);
-  //   if (typeof newDepositAmount === "number") {
+  if (isNaN(newDepositAmount)) {
+    var audio = document.getElementById("wrong");
+    audio.play();
+    alert("Please provide a valid number");
+    depositField.value = "";
+    return;
+  }
   // step-3: get the current deposit total
-  // for non-input(element other than input, textarea) use innerText to get the text
+
   const depositTotalElement = document.getElementById("deposit-total");
   const previousDepositTotalString = depositTotalElement.innerText;
   const previousDepositTotal = parseFloat(previousDepositTotalString);
@@ -34,9 +38,6 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
 
   // step-7: clear the deposit field
   document.getElementById("deposit-field").value = "";
-  //   } else {
-  //     alert("please deposit a valid amount");
-  //   }
 });
 
 // ----------------------------
@@ -45,34 +46,31 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
 document
   .getElementById("deposit-field")
   .addEventListener("keypress", function (event) {
-    // step-2: get the deposit amount from the deposit input field
-    // For input field use .value to the the value inside the input field
     if (event.key === "Enter") {
       const depositField = document.getElementById("deposit-field");
       const newDepositAmountString = depositField.value;
       const newDepositAmount = parseFloat(newDepositAmountString);
-      //   const type = typeof newDepositAmount;
-      //   console.log(type);
-      //   if (typeof newDepositAmount === "number") {
-      // step-3: get the current deposit total
-      // for non-input(element other than input, textarea) use innerText to get the text
+      if (isNaN(newDepositAmount)) {
+        var audio = document.getElementById("wrong");
+        audio.play();
+        alert("Please provide a valid number");
+        depositField.value = "";
+        return;
+      }
       const depositTotalElement = document.getElementById("deposit-total");
       const previousDepositTotalString = depositTotalElement.innerText;
       const previousDepositTotal = parseFloat(previousDepositTotalString);
 
-      // step-4: add numbers to set the total deposit
       const currentDepositTotal = previousDepositTotal + newDepositAmount;
-      // set the deposit total
+
       depositTotalElement.innerText = currentDepositTotal;
 
-      // step-5: get ballance current total
       const balanceTotalElement = document.getElementById("balance-total");
       const previousBalanceTotalString = balanceTotalElement.innerText;
       const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
-      // step-6: calculate current total balance
       const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
-      // set the balance total
+
       balanceTotalElement.innerText = currentBalanceTotal;
 
       var audio = document.getElementById("audio");
@@ -81,7 +79,4 @@ document
       // step-7: clear the deposit field
       document.getElementById("deposit-field").value = "";
     }
-    //   } else {
-    //     alert("please deposit a valid amount");
-    //   }
   });
--- withdraw.js
+++ withdraw.js
@@ -1,87 +1,88 @@
-// step-1: add event listener to the deposit button
 document.getElementById("btn-withdraw").addEventListener("click", function () {
-  // step-2: get the deposit amount from the deposit input field
-  // For input field use .value to the the value inside the input field
   const withdrawField = document.getElementById("withdraw-field");
   const newWithdrawAmountString = withdrawField.value;
   const newWithdrawAmount = parseFloat(newWithdrawAmountString);
-  //   const type = typeof newWithdrawAmount;
-  //   console.log(type);
-  //   if (typeof newWithdrawAmount === "number") {
-  // step-3: get the current deposit total
-  // for non-input(element other than input, textarea) use innerText to get the text
+  if (isNaN(newWithdrawAmount)) {
+    var audio = document.getElementById("wrong");
+    audio.play();
+    alert("Please provide a valid number");
+    withdrawField.value = "";
+    return;
+  }
+
   const withdrawTotalElement = document.getElementById("withdraw-total");
   const previousWithdrawTotalString = withdrawTotalElement.innerText;
   const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-  // step-4: add numbers to set the total deposit
   const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-  // set the deposit total
+
   withdrawTotalElement.innerText = currentWithdrawTotal;
 
-  // step-5: get ballance current total
   const balanceTotalElement = document.getElementById("balance-total");
   const previousBalanceTotalString = balanceTotalElement.innerText;
   const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
-  // step-6: calculate current total balance
+  if (newWithdrawAmount > previousBalanceTotal) {
+    var audio = document.getElementById("wrong");
+    audio.play();
+    document.getElementById("withdraw-field").value = "";
+    alert("You don't have enough balance");
+    return;
+  }
   const currentBalanceTotal = previousBalanceTotal - newWithdrawAmount;
-  // set the balance total
+
   balanceTotalElement.innerText = currentBalanceTotal;
 
   var audio = document.getElementById("audio");
   audio.play();
 
-  // step-7: clear the deposit field
   document.getElementById("withdraw-field").value = "";
-  //   } else {
-  //     alert("please deposit a valid amount");
-  //   }
 });
 
 // ----------------------------
 
-// step-1: add event listener to the deposit button
 document
   .getElementById("withdraw-field")
   .addEventListener("keypress", function (event) {
-    // step-2: get the deposit amount from the deposit input field
-    // For input field use .value to the the value inside the input field
     if (event.key === "Enter") {
       const withdrawField = document.getElementById("withdraw-field");
       const newWithdrawAmountString = withdrawField.value;
       const newWithdrawAmount = parseFloat(newWithdrawAmountString);
-      //   const type = typeof newWithdrawAmount;
-      //   console.log(type);
-      //   if (typeof newWithdrawAmount === "number") {
-      // step-3: get the current deposit total
-      // for non-input(element other than input, textarea) use innerText to get the text
+      if (isNaN(newWithdrawAmount)) {
+        var audio = document.getElementById("wrong");
+        audio.play();
+        alert("Please provide a valid number");
+        withdrawField.value = "";
+        return;
+      }
+
       const withdrawTotalElement = document.getElementById("withdraw-total");
       const previousWithdrawTotalString = withdrawTotalElement.innerText;
       const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-      // step-4: add numbers to set the total deposit
       const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-      // set the deposit total
+
       withdrawTotalElement.innerText = currentWithdrawTotal;
 
-      // step-5: get ballance current total
       const balanceTotalElement = document.getElementById("balance-total");
       const previousBalanceTotalString = balanceTotalElement.innerText;
       const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
-      // step-6: calculate current total balance
+      if (newWithdrawAmount > previousBalanceTotal) {
+        var audio = document.getElementById("wrong");
+        audio.play();
+        document.getElementById("withdraw-field").value = "";
+        alert("You don't have enough balance");
+        return;
+      }
+
       const currentBalanceTotal = previousBalanceTotal - newWithdrawAmount;
-      // set the balance total
+
       balanceTotalElement.innerText = currentBalanceTotal;
 
       var audio = document.getElementById("audio");
       audio.play();
 
-      // step-7: clear the deposit field
       document.getElementById("withdraw-field").value = "";
     }
-    //   } else {
-    //     alert("please deposit a valid amount");
-    //   }
   });
